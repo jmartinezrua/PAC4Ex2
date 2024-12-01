a53ba6051b3acfd9afeea6240db628862afcd5c7
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 09:57:55 +0100

    Update ActivityProgramming.java
    08

--- src/main/java/edu/uoc/pac4/activity/ActivityProgramming.java
+++ src/main/java/edu/uoc/pac4/activity/ActivityProgramming.java
@@ -1,18 +1,38 @@
-// ActivityProgramming.java
 package edu.uoc.pac4.activity;
 
+/**
+ * Abstract class representing a programming activity that can be evaluated.
+ */
 public abstract class ActivityProgramming extends Activity implements Evaluable {
     private double weight;
 
+    /**
+     * Constructs a new ActivityProgramming.
+     *
+     * @param name        the name of the activity
+     * @param description the description of the activity
+     * @param weight      the weight of the activity
+     * @throws ActivityException if the name is invalid
+     */
     public ActivityProgramming(String name, String description, double weight) throws ActivityException {
         super(name, description);
         setWeight(weight);
     }
 
+    /**
+     * Gets the weight of the activity.
+     *
+     * @return the weight of the activity
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Sets the weight of the activity.
+     *
+     * @param weight the new weight of the activity
+     */
     public void setWeight(double weight) {
         if (weight < 0 || weight > 100) {
             weight = Math.max(0, Math.min(weight, 100));
