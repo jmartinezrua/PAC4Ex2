c237d489c8ebfcc2987e131a7ac128c9acc6b031
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 09:55:35 +0100

    Update Activity.java
    06

--- src/main/java/edu/uoc/pac4/activity/Activity.java
+++ src/main/java/edu/uoc/pac4/activity/Activity.java
@@ -3,21 +3,42 @@ package edu.uoc.pac4.activity;
 import edu.uoc.pac4.execution.Execution;
 import java.util.LinkedList;
 
+/**
+ * Abstract class representing an Activity.
+ */
 public abstract class Activity implements Cloneable {
     private String name;
     private String description;
     private LinkedList<Execution> executions;
 
+    /**
+     * Constructs a new Activity.
+     *
+     * @param name        the name of the activity
+     * @param description the description of the activity
+     * @throws ActivityException if the name is invalid
+     */
     public Activity(String name, String description) throws ActivityException {
         setName(name);
         setDescription(description);
         this.executions = new LinkedList<>();
     }
 
+    /**
+     * Gets the name of the activity.
+     *
+     * @return the name of the activity
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the activity.
+     *
+     * @param name the new name of the activity
+     * @throws ActivityException if the name is null or empty
+     */
     public void setName(String name) throws ActivityException {
         if (name == null || name.trim().isEmpty()) {
             throw new ActivityException(ActivityException.INVALID_NAME);
@@ -25,10 +46,20 @@ public abstract class Activity implements Cloneable {
         this.name = name.trim();
     }
 
+    /**
+     * Gets the description of the activity.
+     *
+     * @return the description of the activity
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the activity.
+     *
+     * @param description the new description of the activity
+     */
     public void setDescription(String description) {
         if (description == null) {
             this.description = "";
@@ -37,10 +68,20 @@ public abstract class Activity implements Cloneable {
         }
     }
 
+    /**
+     * Adds an execution to the activity.
+     *
+     * @param execution the execution to add
+     */
     public void addExecution(Execution execution) {
         this.executions.add(execution);
     }
 
+    /**
+     * Gets the list of executions.
+     *
+     * @return the list of executions
+     */
     public LinkedList<Execution> getExecutions() {
         return executions;
     }
